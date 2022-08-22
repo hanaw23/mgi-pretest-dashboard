8ac721678eafd1827e3a7c7b7906765fe1eeefce
Hana Widyatari <77064807+hanaw23@users.noreply.github.com>
2022-08-22 21:24:30 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+<h1 classname="text-rose-500">Hallo gaes im hana</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
