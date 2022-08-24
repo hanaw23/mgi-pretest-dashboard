dd3e1930263462cf4f584f129c8508cb63dd45e3
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-24 08:28:11 +0700

    fixing menubar

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import SkybuildIcon from "./svg/menubarIcons/SkybuildIcon";
 import HeaderIcon from "./svg/menubarIcons/HeaderIcon";
 import { firstMenuIcon, secondMenuIcon } from "../utilities/IconList";
 
@@ -14,28 +13,42 @@ export default function MenuBar() {
   return (
     <div className="bg-white flex flex-col">
       <div className="mt-8 mx-2">
-        {/* <img src="assets/logo.png" height={70} width={70} /> */}
-        <h1 className="">Logo</h1>
+        {/* <img src="assets/logo.png" height={100} width={100} /> */}
+        <h1 className="ml-1">Logo</h1>
       </div>
 
-      <div className="mt-16 mx-auto">
+      <div className="mt-16 -mr-[20px]">
         {firstMenuIcon.map((item, i) => {
-          return <div key={i}>{item.icon}</div>;
+          return (
+            <div
+              key={i}
+              className={`cursor-pointer pl-6 hover:bg-[#e2efe1] hover:border-l-[5px] hover:border-[#69aa64] py-1 ${item.id === 5 ? "bg-[#e2efe1] " : "bg-transparent"} ${item.id === 5 ? " border-l-[5px] " : "border-0"} ${
+                item.id === 5 ? " border-[#69aa64]" : "border-transparent"
+              }`}
+            >
+              {!item.link ? (
+                item.icon
+              ) : (
+                <div onClick={handleClick} className="py-3">
+                  <a href={item.link}>{item.icon}</a>
+                </div>
+              )}
+            </div>
+          );
         })}
-        <div className={`cursor-pointer ${isClick ? "bg-[#abdda7]" : "bg-transparent"}  z-30 h-[30px] hover:bg-[#abdda7]`} onClick={handleClick}>
-          <a href="/company">
-            <SkybuildIcon height={20} width={20} />
-          </a>
-        </div>
       </div>
 
-      <div className="mx-auto mt-16">
+      <div className="mt-16 -mr-[20px]">
         {secondMenuIcon.map((item, i) => {
-          return <div key={i}>{item.icon}</div>;
+          return (
+            <div key={i} className="cursor-pointer pl-6 py-1 hover:bg-[#e2efe1] hover:border-l-[5px] hover:border-[#69aa64]">
+              {item.icon}
+            </div>
+          );
         })}
       </div>
-      <div className="mx-auto mt-12">
-        <HeaderIcon height={20} width={20} fill="#767676" className="mb-8" />
+      <div className="mt-12 cursor-pointer -mr-[20px] pl-6 py-1 hover:bg-[#e2efe1] hover:border-l-[5px] hover:border-[#69aa64]">
+        <HeaderIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />
       </div>
     </div>
   );
--- utilities/IconList.js
+++ utilities/IconList.js
@@ -2,6 +2,7 @@ import BoxIcon from "../components/svg/menubarIcons/BoxIcon";
 import CheckIcon from "../components/svg/menubarIcons/CheckIcon";
 import TicketIcon from "../components/svg/menubarIcons/TicketIcon";
 import HomeIcon from "../components/svg/menubarIcons/HomeIcon";
+import SkybuildIcon from "../components/svg/menubarIcons/SkybuildIcon";
 
 import BoxCloseIcon from "../components/svg/menubarIcons/BoxCloseIcon";
 import MemberIcon from "../components/svg/menubarIcons/MemberIcon";
@@ -12,45 +13,50 @@ import RectangleIcon from "../components/svg/menubarIcons/RectangleIcon";
 export const firstMenuIcon = [
   {
     id: 1,
-    icon: <HomeIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <HomeIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
   },
   {
     id: 2,
-    icon: <TicketIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <TicketIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
   },
   {
     id: 3,
     icon: (
-      <div className="mb-8">
+      <div className="mb-4 mt-2">
         <CheckIcon height={20} width={20} fill="#767676" />
       </div>
     ),
   },
   {
     id: 4,
-    icon: <BoxIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <BoxIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
+  },
+  {
+    id: 5,
+    icon: <SkybuildIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
+    link: "/company",
   },
 ];
 
 export const secondMenuIcon = [
   {
     id: 1,
-    icon: <MemberIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <MemberIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
   },
   {
     id: 2,
-    icon: <LayerIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <LayerIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
   },
   {
     id: 3,
-    icon: <BoxCloseIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <BoxCloseIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
   },
   {
     id: 4,
-    icon: <RectangleIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <RectangleIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
   },
   {
     id: 5,
-    icon: <SearchDollarIcon height={20} width={20} fill="#767676" className="mb-8" />,
+    icon: <SearchDollarIcon height={20} width={20} fill="#767676" className="mb-4 mt-2" />,
   },
 ];
